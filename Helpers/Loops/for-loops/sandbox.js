1b4cb82f4a8b757eb1565d33719fb4f8e3e1e0d8
Peter SImmons <peterorpete@me.com>
2021-06-21 14:14:33 +0100

    slight tweak to for loop lesson

--- Helpers/Loops/for-loops/sandbox.js
+++ Helpers/Loops/for-loops/sandbox.js
@@ -6,12 +6,16 @@ console.log("loop finished");
 
 const names = ["shaun", "mario", "luigi"];
 
+//////
+
+let html = "";
+
 for (let i = 0; i < names.length; i++) {
   console.log(i); //0, 1, 2
   console.log(names[i]); //shaun, mario, luigi
-  let html = `
-  <div>
-  ${names[i]}
-  <div>`;
-  console.log(html); //shaun, mario, luigi
+  html += `<div>${names[i]}<div>`;
 }
+
+console.log(html); //<div>shaun<div><div>mario<div><div>luigi<div>
+
+//////
