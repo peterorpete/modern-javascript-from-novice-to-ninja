7fefa42459e693a3a4bd2f0d9eeb6bb4dde47e6d
Peter SImmons <peterorpete@me.com>
2021-07-13 22:21:42 +0100

    while loop

---
+++ Helpers/Loops/while-loops/sandbox.js
@@ -0,0 +1,11 @@
+let i = 0;
+while (i < 5) {
+  console.log("in loop", i);
+  i++;
+}
+let j = 0;
+
+const names = ["pete", "mario", "luigi"];
+while (j < names.length) {
+  console.log(names[j]);
+}
