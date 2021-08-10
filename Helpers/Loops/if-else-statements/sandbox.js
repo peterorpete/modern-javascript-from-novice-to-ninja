e51bd6a9b8a1dde2066da76bae1b743696b5ea08
Pete <peterorpete@me.com>
2021-08-10 19:26:19 +0100

    added else if staments

---
+++ Helpers/Loops/if-else-statements/sandbox.js
@@ -0,0 +1,11 @@
+// else if statements
+
+const password = "pass";
+
+if (password.length >= 12) {
+ console.log("that password is very strong");
+} else if (password.length >= 8) {
+ console.log("that password is long enough");
+} else {
+ console.log("that password is not long enough");
+}
