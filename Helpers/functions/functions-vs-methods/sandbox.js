08d3bb070916162be110d97d38631fb93c8e83b1
Pete <peterorpete@me.com>
2021-08-11 20:08:06 +0100

    functions vs methods

---
+++ Helpers/functions/functions-vs-methods/sandbox.js
@@ -0,0 +1,12 @@
+const name = "pete";
+
+//functions
+const greet = () => "hello";
+
+let resultOne = greet();
+console.log(resultOne);
+
+//methods
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
