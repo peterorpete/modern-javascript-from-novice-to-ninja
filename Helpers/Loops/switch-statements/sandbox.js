002838f66b49f24e3b8ca38ed2782054a787b9f3
Pete <peterorpete@me.com>
2021-08-11 14:11:10 +0100

    switch statements

---
+++ Helpers/Loops/switch-statements/sandbox.js
@@ -0,0 +1,26 @@
+// switch statements
+const grade = "D";
+
+// without the breaks it would carry going through each case
+switch (grade) {
+ case "A":
+  console.log("Congrats you got an A!");
+  break;
+ case "B":
+  console.log("you got a B!");
+  break;
+ case "C":
+  console.log("you got a C!");
+  break;
+ case "D":
+  console.log("you got a D!");
+  break;
+ case "E":
+  console.log("Sorry you got an E!");
+  break;
+ default:
+  console.log("not a valid grade");
+}
+
+// using if statements
+// if(grade === 'A'){
