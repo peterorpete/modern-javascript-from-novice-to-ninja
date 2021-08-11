228e8db68a72e90901ca755e768f4e68064af7b2
Pete <peterorpete@me.com>
2021-08-11 13:41:23 +0100

    logical NOT lesson done

---
+++ Helpers/Loops/logical-NOT/sandbox.js
@@ -0,0 +1,11 @@
+// Logcal NOT (!)
+
+let user = false;
+
+if (!user) {
+ // do something is user is false
+ console.log("you must be logged into the console.");
+}
+
+console.log(!true); //false
+console.log(!false); //true
