41c4c56fccbef11ec873697bc0619f1fe2e82256
Pete <peterorpete@me.com>
2021-08-15 12:53:40 +0100

    changing css styles

--- Helpers/document-object-model/getting-and-setting-attributes/sandbox.js
+++ Helpers/document-object-model/getting-and-setting-attributes/sandbox.js
@@ -7,5 +7,7 @@ link.textContent = "The Net Ninja webiste";
 const mssg = document.querySelector("p");
 
 console.log(mssg.getAttribute("class"));
+
+// only problem with setAttribute it overides whats was there before.
 mssg.setAttribute("class", "success");
 mssg.setAttribute("style", "color: green");
