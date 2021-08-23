88caa94d8c99202cf5e902f6e2ba253ca1a2a770
Pete <peterorpete@me.com>
2021-08-23 11:51:14 +0100

    regular expressions

---
+++ Helpers/forms/regular-expressions/sandbox.js
@@ -0,0 +1,26 @@
+// https://regex101.com/
+// https://www.youtube.com/playlist?list=PL4cUxeGkcC9g6m_6Sld9Q4jzqdqHd2HiD
+
+const form = document.querySelector(".signup-form");
+
+form.addEventListener("submit", (e) => {
+ e.preventDefault();
+ console.log(form.username.value);
+});
+
+// testing RegEx
+
+const username = "shaunyp";
+const pattern = /^[a-z]{6,}$/;
+
+let result = pattern.test(username);
+
+// if(result){
+//   console.log('regex test passed :)');
+// } else {
+//   console.log('regex test failed :(');
+// }
+
+let result = username.search(pattern);
+
+console.log(result);
