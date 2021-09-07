cae838cb4611b9fc7cfdbf16448c470711c6053f
Pete <peterorpete@me.com>
2021-09-07 23:24:50 +0100

    todo gets added

---
+++ projects/todo-list/app.js
@@ -0,0 +1,22 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = (todo) => {
+ let html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+     <span>${todo}</span>
+     <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+ list.innerHTML += html;
+};
+addForm.addEventListener("submit", (e) => {
+ e.preventDefault();
+ const todo = addForm.add.value.trim();
+ //  console.log(todo);
+ if (todo.length) {
+  //if a postive number its going to equal true
+  generateTemplate(todo);
+ }
+ addForm.reset(); // clears the previous forms input
+});
