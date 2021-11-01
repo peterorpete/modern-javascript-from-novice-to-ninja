635e342a55959aa7d5b4fa58cf5fd097f33499f8
Pete <peterorpete@me.com>
2021-11-01 16:06:41 +0000

    updating ui whgen change weather

--- projects/weather_app/scripts/app.js
+++ projects/weather_app/scripts/app.js
@@ -1,4 +1,32 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+ const cityDets = data.cityDets;
+ const weather = data.weather;
+
+ //update details template
+ details.innerHTML = `
+//  <img src="https://www.accuweather.com/images/weathericons/0${weather.WeatherIcon}.svg" class="time card-img-top" />
+ <div class="icon bg-light mx-auto text-center">
+  <!-- icon here later -->
+ </div>
+ <h5 class="my-3">${cityDets.LocalizedName}</h5>
+ <div class="my-3">${weather.WeatherText}</div>
+ <div class="display-4 my-4">
+   <span>temp</span>
+   <span>${weather.Temperature.Metric.Value}.&deg;C</span>
+ </div>
+ `;
+ console.log(weather);
+
+ // remove the d-none class if present
+ if (card.classList.contains("d-none")) {
+  card.classList.remove("d-none");
+ }
+};
+
 const updateCity = async (city) => {
  //  console.log(city);
  const cityDets = await getCity(city);
@@ -19,6 +47,6 @@ cityForm.addEventListener("submit", (e) => {
 
  //update the ui with new city
  updateCity(city)
-  .then((data) => console.log(data))
+  .then((data) => updateUI(data))
   .catch((err) => console.log(err));
 });
