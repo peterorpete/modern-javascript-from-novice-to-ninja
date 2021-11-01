1a7a6c6e44ecc18a8e8d26c7e874fb3f9d4eba18
Pete <peterorpete@me.com>
2021-11-01 16:32:14 +0000

    made the weather icon update

--- projects/weather_app/scripts/app.js
+++ projects/weather_app/scripts/app.js
@@ -1,13 +1,17 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+// <img src="https://via.placeholder.com/400x300" class="time card-img-top" />
+// <div class="icon bg-light mx-auto text-center">
 
 const updateUI = (data) => {
  const { cityDets, weather } = data;
 
  //update details template
  details.innerHTML = `
-
  <h5 class="my-3">${cityDets.LocalizedName}</h5>
  <div class="my-3">${weather.WeatherText}</div>
  <div class="display-4 my-4">
@@ -15,6 +19,17 @@ const updateUI = (data) => {
    <span>${weather.Temperature.Metric.Value}.&deg;C</span>
  </div>
  `;
+
+ // update the night/day and icon images
+ let timeSrc = "";
+ if (weather.IsDayTime === true) {
+  timeSrc = `img/day.svg`;
+ } else {
+  timeSrc = `img/night.svg`;
+ }
+ let iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+ time.setAttribute("src", timeSrc);
+ icon.setAttribute("src", iconSrc);
  console.log(weather);
 
  // remove the d-none class if present
