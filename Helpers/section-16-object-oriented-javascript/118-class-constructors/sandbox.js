92c5482704e39605ee0ab0aa2973089d52ff04a8
Peter Simmons <peter.simmons@torfx.com>
2021-11-04 10:20:31 +0000

    class constructors

---
+++ Helpers/section-16-object-oriented-javascript/118-class-constructors/sandbox.js
@@ -0,0 +1,19 @@
+class User {
+  constructor(username, email) {
+    // set up properties
+    this.username = username;
+    this.email = email;
+  }
+}
+
+const userOne = new User('mario', 'mario@email.com');
+const userTwo = new User('luigi',  'luigi@email.com');
+
+console.log(userOne);
+console.log(userTwo);
+
+// the 'new' key word
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructior function to 'build' the object
+
