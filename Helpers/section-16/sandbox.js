993a2fe0765ff6cda5c0e4cdb573efc04a7908bc
Pete <peterorpete@me.com>
2021-11-12 18:59:29 +0000

    made it so you can delete a recipe from the database

--- Helpers/section-16/sandbox.js
+++ Helpers/section-16/sandbox.js
@@ -1,30 +1,61 @@
 const list = document.querySelector("ul");
-const addRecipe = (recipe) => {
+const form = document.querySelector("form");
+const addRecipe = (recipe, id) => {
  let time = recipe.created_at.toDate();
  let html = `
-  <li>
+  <li data-id='${id}'>
     <div>
     ${recipe.title}
     </div>
     <div>
     ${time}
     </div>
+    <button class="btn btn-danger btn-sm my-2">delete</button>
   </li>
  `;
  list.innerHTML += html;
 };
+// get documents
 db
  .collection("recipes")
  .get()
  .then((snapshot) => {
   //when we have the data
-  // console.log(snapshot.docs[0].data());
   snapshot.docs.forEach((doc) => {
    console.log(doc.data().created_at);
-   //  console.log(doc.data().created_at.toDate());
-   addRecipe(doc.data());
+   addRecipe(doc.data(), doc.id);
   });
  })
  .catch((err) => {
   console.log(err);
  });
+
+// add documents
+form.addEventListener("submit", (e) => {
+ e.preventDefault();
+ const now = new Date();
+ const recipe = {
+  title: form.recipe.value,
+  created_at: firebase.firestore.Timestamp.fromDate(now),
+ };
+ db
+  .collection("recipes")
+  .add(recipe)
+  .then(() => {
+   console.log("recipe added");
+  })
+  .catch(() => {
+   console.log(error);
+  });
+ console.log(recipe);
+});
+
+//deleting data
+list.addEventListener("click", (e) => {
+ console.log(e.target.tagName);
+ if (e.target.tagName === "BUTTON") {
+  const id = e.target.parentElement.getAttribute("data-id");
+  console.log(id);
+  db.collection("recipes").doc(id).delete().then(console.log("recipe deleted"));
+ }
+});
