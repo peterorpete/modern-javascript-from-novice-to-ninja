1886503ef08e8598a6034b22b15c87b0b64387df
Pete <peterorpete@me.com>
2021-11-12 18:31:30 +0000

    connected to database and displayed the items in the database

--- Helpers/section-16/sandbox.js
+++ Helpers/section-16/sandbox.js
@@ -1,46 +1,30 @@
-// https://www.udemy.com/course/modern-javascript-from-novice-to-ninja/learn/lecture/14241486#overview
-
-class User {
-  constructor(username, email) {
-    // set up properties
-    this.username = username;
-    this.email = email;
-    this.score = 0;
-  }
-  // dont use arrow functions as arrow functions dont bind the this keyword
-  login() {
-    console.log(`${this.username} just logged in`);
-    return this;
-  }
-  logout() {
-    console.log(`${this.username} just logged out`);
-    return this;
-  }
-  incScore() {
-    this.score ++;
-    console.log(`${this.username} has a score of ${this.score}`);
-    return this;
-  }
-}
-
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter((u) => {
-      return u.username !== user.username;
-    });
-  }
-}
-
-const userOne = new User('mario', 'mario@email.com');
-const userTwo = new User('luigi',  'luigi@email.com');
-const userThree = new Admin('Peter',  'peter@email.com');
-
-console.log(userOne);
-console.log(userTwo);
-console.log(userThree);
-
-let users = [userOne, userTwo, userThree];
-console.log(users);
-
-userThree.deleteUser(userTwo);
-console.log(users);
+const list = document.querySelector("ul");
+const addRecipe = (recipe) => {
+ let time = recipe.created_at.toDate();
+ let html = `
+  <li>
+    <div>
+    ${recipe.title}
+    </div>
+    <div>
+    ${time}
+    </div>
+  </li>
+ `;
+ list.innerHTML += html;
+};
+db
+ .collection("recipes")
+ .get()
+ .then((snapshot) => {
+  //when we have the data
+  // console.log(snapshot.docs[0].data());
+  snapshot.docs.forEach((doc) => {
+   console.log(doc.data().created_at);
+   //  console.log(doc.data().created_at.toDate());
+   addRecipe(doc.data());
+  });
+ })
+ .catch((err) => {
+  console.log(err);
+ });
